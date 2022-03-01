e362bb7d20b2d479a1987bbde9619a8dffde5f45
techlm77 <dominicyoungjackson@gmail.com>
2022-03-01 03:40:00 +0000

    Update script.js

--- JS HTML DOM/Creating Elements/script.js
+++ JS HTML DOM/Creating Elements/script.js
@@ -1,14 +1,54 @@
 function selectProject() {
   const body = document.body;
+
+  const projectbody = document.createElement("div");
+  body.append(projectbody);
+
+  const navbar = document.createElement("div");
+  projectbody.append(navbar);
+
+  const left = document.createElement("div");
+  navbar.append(left);
+
+  const ultopnav = document.createElement("ul");
+  left.append(ultopnav);
+
+  const licurrentpage = document.createElement("li");
+  ultopnav.append(licurrentpage);
+
+  const selectpage = [
+    "Home",
+    "Projects",
+    "About",
+    "Contact"
+  ];
+
+  for (let currentpage of selectpage) {
+    let selectedpage = document.createElement("li");
+    selectedpage.textContent = currentpage;
+
+    licurrentpage.appendChild(selectedpage);
+  };
+
   const menu = document.createElement("div");
+  left.append(menu);
+
   const project = document.createElement("div");
+  menu.append(project);
+
   const select = document.createElement("div");
+  project.append(select);
 
   const heading = document.createElement("h1");
   heading.textContent = "projects";
+  select.append(heading);
 
   const ulist = document.createElement("ul");
+  select.append(ulist);
+
   const litem = document.createElement("li");
+  ulist.append(litem);
+
   const projectlist = [
     "3D Shapes",
     "Design",
@@ -27,14 +67,11 @@ function selectProject() {
     litem.appendChild(newprolist);
   };
 
-  ulist.append(litem);
-  select.append(heading);
-  select.append(ulist);
-  project.append(select);
-  menu.append(project);
-  body.append(menu);
+  document.getElementsByTagName("div")[0].className = "project";
+  document.getElementsByTagName("div")[1].className = "navbar";
+  document.getElementsByTagName("div")[2].className = "left";
 
-  document.getElementsByTagName("div")[0].className = "menu";
-  document.getElementsByTagName("div")[1].className = "project";
-  document.getElementsByTagName("div")[2].className = "select";
+  document.getElementsByTagName("div")[3].className = "menu";
+  document.getElementsByTagName("div")[4].className = "project";
+  document.getElementsByTagName("div")[5].className = "select";
 }
